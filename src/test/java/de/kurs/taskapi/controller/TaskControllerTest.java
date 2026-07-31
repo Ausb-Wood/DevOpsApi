ff0d803f1b1072dd755e4e575538a46c9a7c366f
Doso <dominik@sostmann.biz>
2026-07-31 11:02:39 +0200

    Update TaskControllerTest.java
    added Vahits tests

--- src/test/java/de/kurs/taskapi/controller/TaskControllerTest.java
+++ src/test/java/de/kurs/taskapi/controller/TaskControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
